f3b3656483fed9f14d4e06868c0c4c75f64ae730
Marko Martinovic <sm.maxel@gmail.com>
2015-03-19 01:32:25 +0100

    listing done through ng-repeat

--- app/components/movies/moviesController.js
+++ app/components/movies/moviesController.js
@@ -8,6 +8,35 @@
 
     function moviesController($scope) {
 
+        // dummy data
+        $scope.movies = [
+            {
+                heading: 'Project One',
+                subheading: 'Subheading',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.'
+            },
+            {
+                heading: 'Project Two',
+                subheading: 'Subheading',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.'
+            },
+            {
+                heading: 'Project Three',
+                subheading: 'Subheading',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!'
+            },
+            {
+                heading: 'Project Four',
+                subheading: 'Subheading',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem officiis illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?'
+            },
+            {
+                heading: 'Project Five',
+                subheading: 'Subheading',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, quo, minima, inventore voluptatum saepe quos nostrum provident ex quisquam hic odio repellendus atque porro distinctio quae id laboriosam facilis dolorum.'
+            }
+        ];
+
     }
 
 }());
