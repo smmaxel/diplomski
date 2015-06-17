1fc55ace0efbc7c6c2f07927e1319801894ae439
Marko Martinovic <sm.maxel@gmail.com>
2015-06-17 22:00:56 +0200

    Admin panel refactored

--- admin/app/components/dashboard/dashboardController.js
+++ admin/app/components/dashboard/dashboardController.js
@@ -8,6 +8,14 @@
 
     function dashboardController($scope, $log) {
 
+        // obtain list of new comments that need our validation
+        $scope.newComments = 26;
+
+        // obtain list of new users
+        $scope.newUsers = 4;
+
+        // users with expired date of registration
+        $scope.invalidUsers = 5;
 
     }
 
